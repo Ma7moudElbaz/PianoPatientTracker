e87b3d02d7de87b73e29fec12a72468e40078c3d
Ma7moudElbaz <dev.mahmoud.elbaz@gmail.com>
2021-02-01 17:02:17 +0200

    Commit 1-2-2021

--- app/src/main/java/com/example/pianopatienttracker/DashboardFragment.java
+++ app/src/main/java/com/example/pianopatienttracker/DashboardFragment.java
@@ -9,8 +9,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 public class DashboardFragment extends Fragment {
+
+    String[] countryNames = {"SA", "SA"};
+    int flags[] = {R.drawable.flag_sa, R.drawable.flag_sa};
+
+    int products[] = {R.drawable.product_jakavi, R.drawable.product_revolade};
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,8 +26,20 @@ public class DashboardFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
 
+    Spinner flagSpinner, productSpinner;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        flagSpinner = view.findViewById(R.id.flagSpinnner);
+        productSpinner = view.findViewById(R.id.productSpinner);
+
+        FlagSpinnerAdapter flagSpinnerAdapter = new FlagSpinnerAdapter(view.getContext(), flags, countryNames);
+        flagSpinner.setAdapter(flagSpinnerAdapter);
+
+        ProductSpinnerAdapter productSpinnerAdapter = new ProductSpinnerAdapter(view.getContext(), products);
+        productSpinner.setAdapter(productSpinnerAdapter);
     }
+
 }
