3de1dfeb2a1165bd7c81eec9ea0bb151178bfa53
Ma7moudElbaz <dev.mahmoud.elbaz@gmail.com>
2021-02-07 14:42:20 +0200

    Commit 7-2-2021

---
+++ app/src/main/java/com/example/pianopatienttracker/rep/Rep_home.java
@@ -0,0 +1,16 @@
+package com.example.pianopatienttracker.rep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pianopatienttracker.R;
+
+public class Rep_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rep_home);
+    }
+}
