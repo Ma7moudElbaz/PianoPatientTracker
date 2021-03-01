e2fa3b0ca72e8538060b35b4c9a5e9f0cafc0878
Ma7moudElbaz <dev.mahmoud.elbaz@gmail.com>
2021-03-01 05:58:26 +0200

    Commit 1-3-2021

---
+++ app/src/main/java/com/cat/pianopatienttracker/rep/home/patients/add_patient/PatientAddAdakveoActivity.java
@@ -0,0 +1,16 @@
+package com.cat.pianopatienttracker.rep.home.patients.add_patient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cat.pianopatienttracker.R;
+
+public class PatientAddAdakveoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_add_adakveo);
+    }
+}
---
+++ app/src/main/java/com/cat/pianopatienttracker/rep/home/patients/add_patient/PatientAddJakaviActivity.java
@@ -0,0 +1,16 @@
+package com.cat.pianopatienttracker.rep.home.patients.add_patient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cat.pianopatienttracker.R;
+
+public class PatientAddJakaviActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_add_jakavi);
+    }
+}
